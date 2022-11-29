5b84fe97113be09250af17c8cd7750623e914098
oshikombo <amukuhupolykarp@gmail.com>
2022-11-29 12:45:57 -0800

    Task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
-for i in range(ord('a'), ord('z') + 1):
-    if i != ord('q') and i != ord('e'):
-        print("{}".format(chr(i)), end='')
+for i in range(ord('a'), ord('{')):
+    if i == 113 or i == 101:
+        continue
+    print("{0:s}".format(chr(i)), end='')
