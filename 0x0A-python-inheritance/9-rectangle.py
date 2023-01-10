3af48d0fedbd2e086c1769276028e5d444f16022
oshikombo <amukuhupolykarp@gmail.com>
2023-01-10 06:38:38 -0800

    Task 8-11

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,28 +1,53 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
-===================================
-module with class BaseGeometry
-===================================
+   6-base_geometry.py module
+   Class BaseGeometry.
+   Public instance method: \
+   def area(self): that raises an Exception with\
+   the message area() is not implemented.
+   Public instance method:\
+   def integer_validator(self, name, value):\
+   that validates value
 """
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+
+    def area(self):
+        """Public instance method: def area(self): \
+           that raises an Exception with\
+           the message area() is not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Public instance method:\
+           def integer_validator(self, name, value):\
+           that validates value"""
+        if type(value) != int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
-
+    """Class Rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
-        """Method for initialized the attrubutes"""
-        self.integer_validator("width", width)
+        """Instantiation with width and height: \
+        def __init__(self, width, height)
+        width and height must be positive integers,\
+        validated by integer_validator"""
+
+        super().integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """Method to redefine a area method in the parent class"""
-
+        """Return the area of the rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
-        """__str__ method for return the next string"""
-
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        """Return the following rectangle description:\
+        [Rectangle] <width>/<height>"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
