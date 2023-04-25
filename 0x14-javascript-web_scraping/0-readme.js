fae4ca850fac9960d93ef79f3740dfc25544ad59
Polykarp Amukuhu <amukuhupolykarp@gmail.com>
2023-04-25 20:30:09 +0000

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-
-const filePath = process.argv[2];
 const fs = require('fs');
-fs.readFile(filePath, 'utf8', function (err, data) {
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
