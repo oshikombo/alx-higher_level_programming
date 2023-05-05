5ed72f4f6b08f60ab383894dc5e7674da0c80c12
Polykarp Amukuhu <amukuhupolykarp@gmail.com>
2023-05-05 07:08:47 +0000

    All Tasks

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+/* a Javascript script that fetches and lists all movies title
+   by using this URL: https://swapi.co/api/films/?format=json
+   All movie titles must be list in the HTML tag UL#list_movies
+   You can’t use document.querySelector to select the HTML tag
+   You must use the jQuery API */
+
+$.get('https://swapi.co/api/films/?format=json', function( data ) {
+  for (let result of data.results) {
+    $('UL#list_movies').append('<li>' + result.title + '</li>');
+  }
+});
